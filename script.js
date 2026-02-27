7151b392c265197ce1c5342f6960e3f61841b142
6eansacue <oscaritojovel@gmail.com>
2026-02-27 18:24:56 -0500

    Section structures complete

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function openMenu() {
-    document.body.classList += " menu--open"
-}
-
-function closeMenu() {
-    document.body.classList.remove('menu--open')
-}
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".menu");
+  const menuOpen = document.querySelector(".header__menu-toggle");
+  const menuClose = document.querySelector(".menu__close");
+  
+  menuOpen.addEventListener("click", () => {
+    menu.classList.toggle("menu--open");
+  });
+  
+  menuClose.addEventListener("click", () => {
+    menu.classList.remove("menu--open");
+  });
+});
